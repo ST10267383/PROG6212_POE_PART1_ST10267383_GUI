f5c0a7818ed623d59b4f431624ac2e67053122d4
Roberto Javier Pena Gonzalez <ST10267383@vcconnect.edu.za>
2024-09-09 21:43:43 +0200

    Further tweaks, also removed a lot of clutter to make the program more simple and easy to use, now ready for references to be put in

--- PROG6212_POE_PART1_ST10267383_GUI/Controllers/ApprovalController.cs
+++ PROG6212_POE_PART1_ST10267383_GUI/Controllers/ApprovalController.cs
@@ -6,7 +6,7 @@ namespace PROG6212_POE_PART1_ST10267383_GUI.Controllers
     {
         public IActionResult VerifyClaim()
         {
-            return View();  // Will render Views/Approval/VerifyClaim.cshtml
+            return View();  
         }
     }
 }
